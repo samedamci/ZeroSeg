87fc875c52d761a9641326fcaa00faa490b50b13
samedamci <samedamci@disroot.org>
2020-03-21 12:34:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup,Extension
+from distutils.core import setup
 setup(
     name = "ZeroSeg",
-    version = "0.0.2",
-    author = "Richard Saville",
-    author_email = "averagemanvspi@gmail.com",
+    version = "0.0.3",
+    author = "Richard Saville, samedamci",
+    author_email = "averagemanvspi@gmail.com, samedamci@disroot.org",
     description = ("Code library for the ZeroSeg 7-segment display add-on board for the Raspberry Pi Zero"),
     license = "MIT",
     keywords = "raspberry pi rpi led max7219 matrix seven segment zeroseg",
-    url = "https://github.com/AverageManVsPi/ZeroSeg",
+    url = "https://github.com/samedamci/ZeroSeg",
     packages=['ZeroSeg'],
 )
