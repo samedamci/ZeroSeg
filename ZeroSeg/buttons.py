1b9a61ef1626e0c99aa44a7b18513eea4ac9648b
samedamci <samedamci@disroot.org>
2020-07-06 13:58:17 +0200

    Improve Button class

--- ZeroSeg/buttons.py
+++ ZeroSeg/buttons.py
@@ -4,27 +4,24 @@ from RPi import GPIO
 
 
 class Button:
-    def __init__(self):
-        self.switch1 = 17
-        self.switch2 = 26
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.switch1, GPIO.IN)
-        GPIO.setup(self.switch2, GPIO.IN)
-
-    def pressed(self, button: str) -> bool:
+    def __init__(self, button: str):
         """
-        Get button status (bool), check if is pressed.
         Accepted values: 'left', 'right'.
         """
         if button == "left":
-            if not GPIO.input(self.switch1):
-                return True
-            else:
-                return False
+            self.button = 17
         elif button == "right":
-            if not GPIO.input(self.switch2):
-                return True
-            else:
-                return False
+            self.button = 26
         else:
             raise ValueError("Invalid button name, allowed: 'left', 'right'.")
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.button, GPIO.IN)
+
+    def pressed(self) -> bool:
+        """
+        Get button status (bool), check if is pressed.
+        """
+        if not GPIO.input(self.button):
+            return True
+        else:
+            return False
