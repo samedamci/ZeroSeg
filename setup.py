bec385ca0b8032df42ed63a8278c4b77c4eb931e
samedamci <samedamci@disroot.org>
2020-07-07 16:39:43 +0200

    Update setup.py
    Change distutils to setuptools and upload package to PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 setup(
-    name = "ZeroSeg",
-    version = "0.0.3",
-    author = "Richard Saville, samedamci",
-    author_email = "averagemanvspi@gmail.com, samedamci@disroot.org",
-    description = ("Code library for the ZeroSeg 7-segment display add-on board for the Raspberry Pi Zero"),
-    license = "MIT",
-    keywords = "raspberry pi rpi led max7219 matrix seven segment zeroseg",
-    url = "https://github.com/samedamci/ZeroSeg",
-    packages=['ZeroSeg'],
+    name="ZeroSeg-Improved",
+    version="0.1.1",
+    author="Richard Saville, samedamci",
+    author_email="samedamci@disroot.org",
+    description=(
+        "Improved code library for the ZeroSeg display board for the Raspberry Pi."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samedamci/ZeroSeg",
+    packages=["ZeroSeg"],
+    license="MIT",
+    keywords="raspberry pi rpi led max7219 matrix seven segment zeroseg",
+    python_requires=">=3.6",
+    install_requires=[
+        'GPIO',
+        'spidev'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
